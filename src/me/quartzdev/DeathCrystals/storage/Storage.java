bf666bd75f71ced1c3bf6f2f965d682c3f234b7b
rockhopper72 <stevenk72@gmail.com>
2014-12-13 18:52:57 -0500

    Started Base for Storage.java

--- src/me/quartzdev/DeathCrystals/storage/Storage.java
+++ src/me/quartzdev/DeathCrystals/storage/Storage.java
@@ -1,14 +1,39 @@
 package me.quartzdev.DeathCrystals.storage;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Storage {
 	
 	// Structure for table: id, expiration_date, inv_contents
+	File storageFile;
 	
-	public Storage(StorageMethod method) {
+	public Storage(File storageFile) {
+		this.storageFile = storageFile;
+		if(!storageFile.exists()){
+			try {
+				storageFile.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public void removeExpired(){
-		
+		try {
+			FileInputStream stream = new FileInputStream(storageFile);
+			InputStreamReader streamReader = new InputStreamReader(stream);
+			BufferedReader in = new BufferedReader(streamReader);
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void loadCrystal(int id) {
