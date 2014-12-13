844d48ac5a124ef8a28847a0f26830363d661a00
QarthO <QarthO@gmail.com>
2014-12-13 15:22:52 -0500

    Read Description
    Removed Branches
    Added plugin.yml and config.yml
    Set foundation for commands

--- src/me/quartzdev/DeathCrystals/Commands.java
+++ src/me/quartzdev/DeathCrystals/Commands.java
@@ -1,5 +1,40 @@
 package me.quartzdev.DeathCrystals;
 
-public class Commands {
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Commands extends JavaPlugin{
+	Plugin plugin;
+	@Override
+	public void onEnable() {
+		super.onEnable();
+	}
+	@Override
+	public void onDisable() {
+		super.onDisable();
+	}
+	
+	String version = plugin.getDescription().getVersion();
+	String Project_Name = "DeathCrystals";
+	String CMD_Label = "deathcrystals";
+	String CMD_Alias = "dc";
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command commandLabel, String label, String[] args) {
+		if(!(sender instanceof Player)){
+			sender.sendMessage(Language.CONSOLE_MESSAGE.getMessage());
+			return super.onCommand(sender, commandLabel, label, args);
+		}
+		Player player = (Player) sender;
+		if(commandLabel.equals(CMD_Label) || commandLabel.equals(CMD_Alias)){
+			if(args.length == 0){
+				player.sendMessage(ChatColor.LIGHT_PURPLE + Project_Name + " v" + version);
+			}
+		}
+		return super.onCommand(sender, commandLabel, label, args);
+	}
 }
--- src/me/quartzdev/DeathCrystals/Language.java
+++ src/me/quartzdev/DeathCrystals/Language.java
@@ -1,5 +1,32 @@
 package me.quartzdev.DeathCrystals;
 
-public class Language {
+import org.bukkit.ChatColor;
+
+public enum Language {
+
+    BORDER_BOTTOM(ChatColor.BLUE + "" + ChatColor.STRIKETHROUGH + "-----------------" + ChatColor.LIGHT_PURPLE + " Created By QarthO " + ChatColor.BLUE + " " + ChatColor.STRIKETHROUGH + " ----------------"),
+	BORDER_TOP(ChatColor.BLUE + "" + ChatColor.STRIKETHROUGH + "------------------" + ChatColor.LIGHT_PURPLE + " Project v%s" + ChatColor.BLUE + " " + ChatColor.STRIKETHROUGH + " ------------------"),
+    CHAT_PREFIX(ChatColor.DARK_GREEN + "[" + ChatColor.BLUE + "Project" + ChatColor.DARK_GREEN + "] " + ChatColor.RESET),	
+	CONSOLE_MESSAGE(ChatColor.RED + "Error: In game command only"),
+    HELP("you need help"),
+	HELP_ADMIN("admin help"),
+	HELP_MOD("mod help"),
+	INCORRECT_USAGE(ChatColor.RED + "Error: Incorrect usage"),
+	NO_PERMISSIONS(ChatColor.RED + "Error: Insuffictient permissions"),
+	PLAYER_NOT_FOUND(ChatColor.RED +  "Error: Player not found"),
+    UKNOWN_COMMAND(ChatColor.RED + "Not a valid command\nTry /dc help");
+	
+	String Message;
+	
+	private Language(String message){
+		this.Message = message;
+	}
+	public String getMessage(){
+		return Message;
+	}
+	public static Language[] getMessages(){
+		Language[] e = {HELP, CHAT_PREFIX};
+		return e;
+	}
 
 }
