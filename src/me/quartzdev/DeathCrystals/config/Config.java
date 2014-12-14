cc8495649900d2be06fe83973671caab78161fc5
rockhopper72 <stevenk72@gmail.com>
2014-12-13 19:55:38 -0500

    Removed unused imports

--- src/me/quartzdev/DeathCrystals/config/Config.java
+++ src/me/quartzdev/DeathCrystals/config/Config.java
@@ -1,7 +1,6 @@
 package me.quartzdev.DeathCrystals.config;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.bukkit.configuration.file.FileConfiguration;
