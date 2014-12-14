e47d62f3773acb4e4b6e3b65588fd3284d44b855
QarthO <QarthO@gmail.com>
2014-12-14 01:28:58 -0500

    Believes(I finished Listeners)

--- src/me/quartzdev/DeathCrystals/Main.java
+++ src/me/quartzdev/DeathCrystals/Main.java
@@ -1,7 +1,10 @@
 package me.quartzdev.DeathCrystals;
 
+import java.io.File;
+
 import me.quartzdev.DeathCrystals.config.Config;
 import me.quartzdev.DeathCrystals.listeners.PlayerListener;
+import me.quartzdev.DeathCrystals.storage.Storage;
 
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -9,7 +12,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin{
 	Plugin plugin;
 	Config config = new Config(this.getConfig());
-	PlayerListener Listener = new PlayerListener(this, config);
+	Storage storage = new Storage(new File("plugins" + File.separator + "DeathCrystals" + File.separator + "deathcrystals.txt"));
+	PlayerListener Listener = new PlayerListener(this, storage, config);
 	@Override
 	public void onEnable() {
 		this.saveDefaultConfig();
