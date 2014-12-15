74292b8a1a066a2edaea9c0bf38cd6ee58ebcb8e
rockhopper72 <stevenk72@gmail.com>
2014-12-14 21:22:17 -0500

    Added InventoryInteract Listener

--- src/me/quartzdev/DeathCrystals/Main.java
+++ src/me/quartzdev/DeathCrystals/Main.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.logging.Logger;
 
 import me.quartzdev.DeathCrystals.config.Config;
+import me.quartzdev.DeathCrystals.listeners.InventoryInteractListener;
 import me.quartzdev.DeathCrystals.listeners.PlayerDeathListener;
 import me.quartzdev.DeathCrystals.listeners.PlayerInteractListener;
 import me.quartzdev.DeathCrystals.storage.Storage;
@@ -12,44 +13,53 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-	private Config config;
-	private Storage storage;
-	private PlayerDeathListener playerDeathListener;
-	private PlayerInteractListener playerInteractListener;
-	private Logger logger;
-	
-	@Override
-	public void onEnable() {
-		config = new Config(this.getConfig());
-		storage = new Storage(new File("plugins" + File.separator + "DeathCrystals" + File.separator + "deathcrystals.txt"));
-		playerDeathListener = new PlayerDeathListener(this, storage, config);
-		playerInteractListener = new PlayerInteractListener(this, storage, config);
-		logger = Bukkit.getLogger();
-		
-		this.saveDefaultConfig();
-		config.loadConfig();
-		Commands commands = new Commands();
-		this.getServer().getPluginCommand("dc").setExecutor(commands);
-		this.getServer().getPluginManager().registerEvents(playerDeathListener, this);
-		this.getServer().getPluginManager().registerEvents(playerInteractListener, this);
-	}
-	
-	@Override
-	public void onDisable() {
-		config.saveConfig();
-	}
-	
-	public void logMessage(String message) {
-		logger.info("[" + this.getName() + "] " + message);
-	}
-	
-	public void errorMessage(String message) {
-		logger.severe("[" + this.getName() + "] " + message);
-	}
-	
-	public void warningMessage(String message) {
-		logger.warning("[" + this.getName() + "] " + message);
-		
-	}
-	
+    private Config config;
+    private Storage storage;
+    private PlayerDeathListener playerDeathListener;
+    private PlayerInteractListener playerInteractListener;
+    private InventoryInteractListener inventoryInteractListener;
+    private Logger logger;
+
+    @Override
+    public void onEnable() {
+	config = new Config(this.getConfig());
+	storage = new Storage(new File("plugins" + File.separator
+		+ "DeathCrystals" + File.separator + "deathcrystals.txt"));
+	playerDeathListener = new PlayerDeathListener(this, storage, config);
+	playerInteractListener = new PlayerInteractListener(this, storage,
+		config);
+	inventoryInteractListener = new InventoryInteractListener(this,
+		storage, config);
+	logger = Bukkit.getLogger();
+
+	this.saveDefaultConfig();
+	config.loadConfig();
+	Commands commands = new Commands();
+	this.getServer().getPluginCommand("dc").setExecutor(commands);
+	this.getServer().getPluginManager()
+		.registerEvents(playerDeathListener, this);
+	this.getServer().getPluginManager()
+		.registerEvents(playerInteractListener, this);
+	this.getServer().getPluginManager()
+		.registerEvents(inventoryInteractListener, this);
+    }
+
+    @Override
+    public void onDisable() {
+	config.saveConfig();
+    }
+
+    public void logMessage(String message) {
+	logger.info("[" + this.getName() + "] " + message);
+    }
+
+    public void errorMessage(String message) {
+	logger.severe("[" + this.getName() + "] " + message);
+    }
+
+    public void warningMessage(String message) {
+	logger.warning("[" + this.getName() + "] " + message);
+
+    }
+
 }
--- src/me/quartzdev/DeathCrystals/listeners/InventoryInteractListener.java
+++ src/me/quartzdev/DeathCrystals/listeners/InventoryInteractListener.java
@@ -3,24 +3,75 @@ package me.quartzdev.DeathCrystals.listeners;
 import me.quartzdev.DeathCrystals.config.Config;
 import me.quartzdev.DeathCrystals.storage.Storage;
 
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.inventory.InventoryInteractEvent;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
-public class InventoryInteractListener {
-	Plugin plugin;
-	Storage storage;
-	Config config;
-	
-	public InventoryInteractListener(Plugin main, Storage storage, Config config) {
-		this.plugin = main;
-		this.config = config;
-		this.storage = storage;
+public class InventoryInteractListener implements Listener {
+    Plugin plugin;
+    Storage storage;
+    Config config;
+
+    public InventoryInteractListener(Plugin main, Storage storage, Config config) {
+	this.plugin = main;
+	this.config = config;
+	this.storage = storage;
+    }
+
+    @EventHandler
+    public void onInventoryInteract(InventoryClickEvent event) {
+	switch (event.getAction()) {
+	case PLACE_ALL:
+	case PLACE_SOME:
+	case PLACE_ONE:
+	case SWAP_WITH_CURSOR:
+	case HOTBAR_SWAP:
+	case UNKNOWN:
+	    if (isCrystalInventory(event.getInventory())) {
+		event.setCancelled(true);
+	    }
+	    break;
+	case MOVE_TO_OTHER_INVENTORY:
+	    if ((event.getRawSlot() <= 45)
+		    && (isCrystalInventory(event.getInventory()))) {
+		int i = getFirstOpenSlot(event.getView().getBottomInventory());
+		if (i == -1) {
+		    event.setCancelled(true);
+		} else {
+		    ItemStack fromItem = event.getView().getBottomInventory()
+			    .getItem(i);
+		    ItemStack toItem = event.getCurrentItem();
+
+		    event.setCancelled(true);
+		    event.setCurrentItem(fromItem);
+		    event.getView().getBottomInventory().setItem(i, toItem);
+		}
+	    }
+	    break;
+	default:
+	    break;
+
 	}
-	
-	@EventHandler
-	public void onInventoryInteract(InventoryInteractEvent event) {
-		// TODO
-		
+    }
+
+    private int getFirstOpenSlot(Inventory inventory) {
+	int i = 0;
+	for (ItemStack item : inventory.getContents()) {
+	    if ((item == null) || (item.equals(null))
+		    || (item.getType().equals(Material.AIR))) {
+		return i;
+	    }
+	    i++;
 	}
+	return -1;
+    }
+
+    private boolean isCrystalInventory(Inventory i) {
+	return i.getName().contains("'s Inventory");
+    }
+
 }
