956d3de1bf62dc9d1ebda404f615be07aa334165
rockhopper72 <stevenk72@gmail.com>
2014-12-14 23:17:17 -0500

    Cleaned Language File

--- src/me/quartzdev/DeathCrystals/Language.java
+++ src/me/quartzdev/DeathCrystals/Language.java
@@ -4,11 +4,20 @@ import org.bukkit.ChatColor;
 
 public enum Language {
 	
-	BORDER_BOTTOM(ChatColor.BLUE + "" + ChatColor.STRIKETHROUGH + "-----------------" + ChatColor.LIGHT_PURPLE + " Created By QarthO " + ChatColor.BLUE + " " + ChatColor.STRIKETHROUGH + " ----------------"), BORDER_TOP(ChatColor.BLUE + ""
-			+ ChatColor.STRIKETHROUGH + "------------------" + ChatColor.LIGHT_PURPLE + " Project v%s" + ChatColor.BLUE + " " + ChatColor.STRIKETHROUGH + " ------------------"), CHAT_PREFIX(ChatColor.DARK_GREEN + "[" + ChatColor.BLUE + "Project"
-			+ ChatColor.DARK_GREEN + "] " + ChatColor.RESET), CONSOLE_MESSAGE(ChatColor.RED + "Error: In game command only"), HELP("you need help"), HELP_ADMIN("admin help"), HELP_MOD("mod help"), INCORRECT_USAGE(ChatColor.RED + "Error: Incorrect usage"), NO_PERMISSIONS(
-			ChatColor.RED + "Error: Insuffictient permissions"), PLAYER_NOT_FOUND(ChatColor.RED + "Error: Player not found"), UKNOWN_COMMAND(ChatColor.RED + "Not a valid command\nTry /dc help"), CRYSTAL_LORE(ChatColor.GREEN
-			+ "Right-Click to Open Inventory"), CRYSTAL_NOT_FOUND(ChatColor.RED + "The crystal with ID %s could not be found."), CRYSTAL_EXPIRED(ChatColor.RED + "The crystal with ID %s expired on %s");
+	BORDER_BOTTOM(ChatColor.BLUE + "" + ChatColor.STRIKETHROUGH + "-----------------" + ChatColor.LIGHT_PURPLE + " Created By QarthO " + ChatColor.BLUE + " " + ChatColor.STRIKETHROUGH + " ----------------"),
+	BORDER_TOP(ChatColor.BLUE + "" + ChatColor.STRIKETHROUGH + "------------------" + ChatColor.LIGHT_PURPLE + " Project v%s" + ChatColor.BLUE + " " + ChatColor.STRIKETHROUGH + " ------------------"),
+	CHAT_PREFIX(ChatColor.DARK_GREEN + "[" + ChatColor.BLUE + "Project" + ChatColor.DARK_GREEN + "] " + ChatColor.RESET),
+	CONSOLE_MESSAGE(ChatColor.RED + "Error: In game command only"),
+	HELP("you need help"),
+	HELP_ADMIN("admin help"),
+	HELP_MOD("mod help"),
+	INCORRECT_USAGE(ChatColor.RED + "Error: Incorrect usage"),
+	NO_PERMISSIONS(ChatColor.RED + "Error: Insuffictient permissions"),
+	PLAYER_NOT_FOUND(ChatColor.RED + "Error: Player not found"),
+	UKNOWN_COMMAND(ChatColor.RED + "Not a valid command\nTry /dc help"),
+	CRYSTAL_LORE(ChatColor.GREEN + "Right-Click to Open Inventory"),
+	CRYSTAL_NOT_FOUND(ChatColor.RED + "The crystal with ID %s could not be found."),
+	CRYSTAL_EXPIRED(ChatColor.RED + "The crystal with ID %s expired on %s");
 	
 	String Message;
 	
